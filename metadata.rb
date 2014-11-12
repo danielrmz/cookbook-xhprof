bb22194cb070cb367c70a0eb0baafffe50158f37
Sebastiaan Provost <sebastiaan.provost@crosscheck.be>
2014-11-12 13:20:43 +0100

    A dependency bugfix.
    A cookbook depends on a cookbook, not on a recipe. Hence that this
    needed to be changed.

--- metadata.rb
+++ metadata.rb
@@ -3,12 +3,11 @@ maintainer_email  "hello@danielrmz.com"
 license           "MIT License"
 description       "Installs xhprof php extension for apache"
 version           "0.0.1"
-depends           "php::source"
+depends           "php"
 depends           "apache2"
- 
+
 recipe "xhprof", "Installs xhprof php extension"
- 
+
 %w{ ubuntu }.each do |os|
 	supports os
 end
-
